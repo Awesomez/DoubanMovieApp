c3b1987b5cbf74099af0f217efaf91be05fe198d
Awesomez <likezyp@gmail.com>
2016-06-10 23:40:35 +0800

    add left menu items

--- app/Home.js
+++ app/Home.js
@@ -4,14 +4,21 @@ import {
     StyleSheet,
     Text,
     DrawerLayoutAndroid,
+    Dimensions,
+    TouchableHighlight,
+    TouchableNativeFeedback,
+    Platform,
     View
 } from 'react-native';
+
+var SCREEN_WIDTH=Dimensions.get('window').width;
+var DRAWER_WIDTH_LEFT=56;
 
 class Home extends Component {
     render() {
         return (
             <DrawerLayoutAndroid
-                drawerWidth={150}
+                drawerWidth={SCREEN_WIDTH-DRAWER_WIDTH_LEFT}
                 drawerPosition={DrawerLayoutAndroid.positions.left}
                 renderNavigationView={() => this.renderDrawer()}>
                 <View style={{flex: 1, alignItems: 'center'}}>
@@ -22,15 +29,35 @@ class Home extends Component {
     }
 
     renderDrawer() {
+        //https://developers.douban.com/wiki/?title=movie_v2
+        var menuList=['正在热映','即将上映','Top250','口碑榜','北美票房榜','新片榜'];
+        var TouchableElement = TouchableHighlight;
+        if (Platform.OS === 'android') {
+            TouchableElement = TouchableNativeFeedback;
+        }
         return (
-            <View style={{flex: 1, backgroundColor: 'green'}}>
-            <Text style={{margin: 10,color:'#fff',fontSize: 15, textAlign: 'center'}}>我是导航功能栏标题</Text>
-            <Text style={{marginTop: 10,marginLeft:20,color:'#fff',fontSize: 15, textAlign: 'left'}}>1.功能1</Text>
-            <Text style={{marginTop: 10,marginLeft:20,color:'#fff',fontSize: 15, textAlign: 'left'}}>2.功能2</Text>
+            <View style={styles.container}>
+                <Text style={{margin: 10,color:'#fff',fontSize: 15, textAlign: 'center'}}>我是导航功能栏标题</Text>
+                {
+                    menuList.map(function (v,k){
+                        return (
+                            <Text key={k} style={{marginTop: 10,marginLeft:20,color:'#fff',fontSize: 15, textAlign: 'left'}}>{v}</Text>)
+                    })
+                }
             </View>
         );
     }
 }
 
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#3E9CE9'
+    },
+    toolbar: {
+        backgroundColor: '#00a2ed',
+        height: 56
+    }
+});
 
 module.exports=Home;
